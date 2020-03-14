e497c5af88b4ce80ce2bee544341fc0131134340
alexislepresle <contact@alexislepresle.com>
2020-03-14 17:21:57 +0100

    first step with api

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,46 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import Api from "../utils/api"
 
-const IndexPage = () => (
+const IndexPage = () => {
+  const {data, loading, error } = Api('https://covid19.mathdro.id/api/confirmed');
+
+  return(
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    {data &&
+    
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Country</th>
+          <th>Case</th>
+          <th>Died</th>
+          <th>Recover</th>
+          <th>Last update</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          console.log("data "+data),
+          data.map((country, code) => (
+            <tr key={code}>
+              <td>{country.countryRegion}</td>
+              <td>{country.confirmed}</td>
+              <td>{country.deaths}</td>
+              <td>{country.recovered}</td>
+            </tr>
+          ))
+        }
+      </tbody>
+    </table>
+    }
+    
+
   </Layout>
-)
+)}
 
 export default IndexPage
---
+++ src/utils/api.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Api(url) {
+  const [data, setData] = useState();
+  const [error, setError] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    async function fetchData() {
+        setIsLoading(true);
+        const result = await axios(url)
+        .catch(error =>setError(error));
+        setData(result.data)
+        setIsLoading(false);
+    }
+    fetchData();
+    }, [url]);
+    return {
+      data,
+      error,
+      isLoading
+  }
+}
+
+export default Api;
